882f2e81c895608a0a8b61c9b0e9121238cb85c4
azdharsyahputra <if23.muhammadsyahputra@mhs.ubpkarawang.ac.id>
2025-09-16 08:35:43 +0700

    data

--- src/data/quests.ts
+++ src/data/quests.ts
@@ -3,24 +3,24 @@ import type { Quest } from "../types/quest";
 export const quests: Quest[] = [
   {
     id: 1,
-    title: "Fix The bug",
-    description: "Fix base url bug at Dynamic Checksheet 5R",
+    title: "Slay the Dragon",
+    description: "Defeat the dragon terrorizing the village.",
     completed: false,
     expReward: 500,
     image: "dragon.jpg", // cukup nama file
   },
   {
     id: 2,
-    title: "Colect API for Dashboard Statistic",
-    description: "I need api for fl statistic and ballmill stat get that information from mas luky",
+    title: "Gather Herbs",
+    description: "Collect 10 healing herbs in the forest.",
     completed: false,
     expReward: 120,
     image: "herbs.jpg",
   },
   {
     id: 3,
-    title: "Fetch API",
-    description: "Sync that api you get to your UI",
+    title: "Escort the Merchant",
+    description: "Protect the merchant traveling to the next town.",
     completed: false,
     expReward: 250,
     image: "merchant.jpg",
